b29cb032bad20d37c216f8783fb00b28791080d1
piaoyu2011 <piaoyu2011@gmail.com>
2024-02-29 12:10:22 +0800

    change replicate to lowercase
    change replicate to lowercase

--- pages/index.js
+++ pages/index.js
@@ -49,13 +49,13 @@ export default function Home() { 
       </Head>
 
       <h1 className="py-6 text-center font-bold text-2xl"> 
-        Dream Sticker with{" "} 
-        <a href="https://replicate.com/fofr/sticker-maker"> 
-         Sticker Maker 
+        Dream Sticker with
+        <a href="https://replicate.com/fofr/sticker-maker">  
+         Sticker Maker  
         </a>
       </h1>
       <h2 className="py-6 text-center font-bold text-xl">  
-          credits to <a href="https://github.com/fofr">fofr</a> and <a href="https://replicate.com/">Replicate</a>
+          credits to <a href="https://github.com/fofr">fofr</a> and <a href="https://replicate.com/">replicate</a>  
       </h2>     
 
       <form className="w-full flex" onSubmit={handleSubmit}> 
